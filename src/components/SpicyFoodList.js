f57d46f59ad3f8645b3afb77fcb425c7a31ab389
winston kiprop <winstonekip50@gmail.com>
2022-07-23 12:17:23 +0300

    Done.

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+ import React, { useState } from "react";
 import { spicyFoods, getNewRandomSpicyFood } from "../data";
+
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
+  
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
     console.log(newFood);
   }
 
